be2adaa19fc7821b88d860cd761b8071d7299804
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-14 01:03:53 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,15 +2,9 @@
 ######################################################################################
 ## Command=wget https://raw.githubusercontent.com/tar1971/kodilite/main/installer1.sh -O - | /bin/sh
 ##
-###########################################
-###########################################
 #!/bin/sh
 echo
-###########################################
-###########################################
 opkg install --force-overwrite  https://github.com/tar1971/kodilite/blob/main/enigma2-plugin-extensions-kodilite_8.3_r0_all.ipk?raw=true
-###########################################
-###########################################
 sleep 2;
 exit 0
 #!/bin/bash
